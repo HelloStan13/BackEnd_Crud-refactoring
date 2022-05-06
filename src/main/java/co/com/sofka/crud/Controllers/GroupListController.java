7521956544a65c4a8f83eba0501943c7f57669ef
Adryan <adryanynfante@gmail.com>
2022-05-05 22:41:27 -0500

    creacion listar
    creacion de servicio listar y controlador listar

--- src/main/java/co/com/sofka/crud/Controllers/GroupListController.java
+++ src/main/java/co/com/sofka/crud/Controllers/GroupListController.java
@@ -1,4 +1,19 @@
+
 package co.com.sofka.crud.Controllers;
 
+import co.com.sofka.crud.Entidades.GroupList;
+import co.com.sofka.crud.DAO.GroupListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class GroupListController {
+    @Autowired
+    private GroupListService service;
+
+    @GetMapping(value = "api/grouplists")
+    public Iterable<GroupList> list() {
+        return service.list();
+    }
 }
--- src/main/java/co/com/sofka/crud/DAO/GroupListService.java
+++ src/main/java/co/com/sofka/crud/DAO/GroupListService.java
@@ -1,4 +1,16 @@
 package co.com.sofka.crud.DAO;
 
+import co.com.sofka.crud.Entidades.GroupList;
+import co.com.sofka.crud.Repositories.GroupListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GroupListService {
+    @Autowired
+    private GroupListRepository repository;
+
+    public Iterable<GroupList> list(){
+        return repository.findAll();
+    }
 }
